1680b6400d80e2e2ea7634ba1e93dbbcb4579f21
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-07 16:17:53 +0200

    feat: add icon section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { IconSection } from './components/icon-section';
 import { SlideTitle } from './components/slide-title';
 
 import './App.css';
 
-const App: React.FC = () => <SlideTitle />;
+const App: React.FC = () => (
+    <>
+        <SlideTitle />
+        <IconSection placeholderIcon="favorite" />
+    </>
+);
 
 export default App;
