098cbbad0bc73c451fd02902fa37779cbed869df
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-08 07:08:57 +0200

    feat: add popover for icon select

--- src/components/icon-section/icon-section.tsx
+++ src/components/icon-section/icon-section.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Popover } from '@mui/material';
 import { EditableText } from '../editable-text';
 import { IconSelector } from '../icon-selector';
 
@@ -9,20 +10,42 @@ interface IconSectionProps {
 export const IconSection: React.FC<IconSectionProps> = ({
     placeholderIcon,
 }) => {
-    const [selectedIcon, setSelectedIcon] = useState(placeholderIcon);
-    const [showIconSelect, setShowIconSelect] = useState(false);
+    const [selectedIcon, setSelectedIcon] = useState<string>(placeholderIcon);
+    const [anchorEl, setAnchorEl] = React.useState<HTMLDivElement | null>(null);
 
-    const handleSelectIcon = (icon: string): void => {
-        setSelectedIcon(icon);
-        setShowIconSelect(false);
+    const openIconSelect = (event: React.MouseEvent<HTMLDivElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const closeIconSelect = () => {
+        setAnchorEl(null);
+    };
+
+    const handleSelectIcon = (iconName: string) => {
+        setSelectedIcon(iconName);
+        closeIconSelect();
     };
 
     return (
         <div>
-            {showIconSelect && <IconSelector onIconSelect={handleSelectIcon} />}
-            <div onClick={() => setShowIconSelect(true)}>
+            <div onClick={openIconSelect}>
                 <span className="material-icons">{selectedIcon}</span>
             </div>
+            <Popover
+                open={Boolean(anchorEl)}
+                anchorEl={anchorEl}
+                onClose={closeIconSelect}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+            >
+                <IconSelector onIconSelect={handleSelectIcon} />
+            </Popover>
             <EditableText placeholderText="Insert text here" />
             <EditableText placeholderText="Add here your additional text" />
         </div>
