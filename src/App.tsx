4e9903aebeb2478afd7e863043c26bdf5a1ed7fb
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-08 07:55:35 +0200

    feat: add layout styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Paper } from '@mui/material';
 import { SlideContent } from './components/slide-content';
 import { SlideTitle } from './components/slide-title';
-import './App.css';
+import './App.scss';
+
+const CN = 'app';
 
 const App: React.FC = () => (
-    <>
-        <SlideTitle />
-        <SlideContent />
-    </>
+    <div className={`${CN}__container`}>
+        <Paper className={`${CN}__slide-card`} elevation={2}>
+            <SlideTitle />
+            <SlideContent />
+        </Paper>
+    </div>
 );
 
 export default App;
