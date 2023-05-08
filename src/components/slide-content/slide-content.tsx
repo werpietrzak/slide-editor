d4a79432448d4f6e555d5f608126b2097293eea7
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-08 08:57:39 +0200

    feat: add drag and drop area component

--- src/components/slide-content/slide-content.tsx
+++ src/components/slide-content/slide-content.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from 'react';
-import {
-    DragDropContext,
-    Draggable,
-    Droppable,
-    OnDragEndResponder,
-} from 'react-beautiful-dnd';
+import React from 'react';
+import { DragAndDropArea } from '../drag-and-drop-area';
 import { IconSection } from '../icon-section';
 import { Icons } from '../../constants';
+import { DraggableItem } from '../../models';
 import './slide-content.scss';
 
-interface DraggableItem {
-    id: string;
-    content: JSX.Element;
-}
-
 export const SlideContent: React.FC = () => {
-    const draggableItems: DraggableItem[] = [
+    const items: DraggableItem[] = [
         {
             id: 'item-1',
             content: <IconSection placeholderIcon={Icons.Favorite} />,
@@ -30,50 +21,11 @@ export const SlideContent: React.FC = () => {
         },
     ];
 
-    const [items, setItems] = useState(draggableItems);
-
-    const handleDragEnd: OnDragEndResponder = result => {
-        if (!result.destination) {
-            return;
-        }
-
-        const newItems = items.slice();
-        const [reorderedItem] = newItems.splice(result.source.index, 1);
-        newItems.splice(result.destination.index, 0, reorderedItem);
-
-        setItems(newItems);
-    };
-
     return (
-        <DragDropContext onDragEnd={handleDragEnd}>
-            <Droppable droppableId="list" direction="horizontal">
-                {provided => (
-                    <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        className="container"
-                    >
-                        {items.map((item, index) => (
-                            <Draggable
-                                key={item.id}
-                                draggableId={item.id}
-                                index={index}
-                            >
-                                {provided => (
-                                    <div
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        ref={provided.innerRef}
-                                    >
-                                        {item.content}
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </DragDropContext>
+        <DragAndDropArea
+            id="slide-content-area"
+            direction="horizontal"
+            draggableItems={items}
+        />
     );
 };
---
+++ src/models/draggable-item.ts
@@ -0,0 +1,4 @@
+export interface DraggableItem {
+    id: string;
+    content: JSX.Element;
+}
