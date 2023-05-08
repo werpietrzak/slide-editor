dcc5e4c65ecf182b63c651885669b535611477f4
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-08 10:59:26 +0200

    feat: add export to pdf functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { Button, Paper } from '@mui/material';
+import jsPDF from 'jspdf';
 import { DragAndDropArea } from './components/drag-and-drop-area';
 import { SlideContent } from './components/slide-content';
 import { SlideTitle } from './components/slide-title';
@@ -20,15 +21,25 @@ const App: React.FC = () => {
         },
     ];
 
+    const generatePDF = () => {
+        const report = new jsPDF('portrait', 'pt', 'a4');
+        report.html(document.getElementById('report')!).then(() => {
+            report.save('report.pdf');
+        });
+    };
+
     return (
         <div className={`${CN}__container`}>
-            <Paper className={`${CN}__slide-card`} elevation={2}>
+            <Paper id="report" className={`${CN}__slide-card`} elevation={2}>
                 <DragAndDropArea
                     id="slide-area"
                     direction="vertical"
                     draggableItems={items}
                 />
             </Paper>
+            <Button onClick={generatePDF} type="button">
+                Export to PDF
+            </Button>
         </div>
     );
 };
