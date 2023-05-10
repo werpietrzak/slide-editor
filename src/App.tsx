119e9581380a1e80addc573fd037ea4144ad4752
Wer Pietrzak <weropietrzak@gmail.com>
2023-05-10 02:10:21 +0200

    fix: remove drag and drop from slide main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
 import React from 'react';
 import { Paper } from '@mui/material';
-import { DragAndDropArea } from './components/drag-and-drop-area';
 import { SlideContent } from './components/slide-content';
 import { SlideTitle } from './components/slide-title';
-import { DraggableItem } from './models';
 import './App.scss';
 
 const CN = 'app';
 
-const App: React.FC = () => {
-    const items: DraggableItem[] = [
-        {
-            id: 'slide-title',
-            content: <SlideTitle />,
-        },
-        {
-            id: 'slide-content',
-            content: <SlideContent />,
-        },
-    ];
-
-    return (
-        <div className={`${CN}__container`}>
-            <Paper className={`${CN}__slide-card`} elevation={2}>
-                <DragAndDropArea
-                    id="slide-area"
-                    direction="vertical"
-                    draggableItems={items}
-                />
-            </Paper>
-        </div>
-    );
-};
+const App: React.FC = () => (
+    <div className={`${CN}__container`}>
+        <Paper className={`${CN}__slide-card`} elevation={2}>
+            <SlideTitle />
+            <SlideContent />
+        </Paper>
+    </div>
+);
 
 export default App;
